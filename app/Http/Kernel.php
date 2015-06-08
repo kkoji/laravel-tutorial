770009d92fe3426a83638ba9695e8b43db67a3a1
Kazuya Kojima <ypcjh662@yahoo.co.jp>
2015-06-08 20:45:06 +0900

    ミドルウェアの登録についてコメント追加

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,6 +6,7 @@ class Kernel extends HttpKernel {
 
 	/**
 	 * The application's global HTTP middleware stack.
+	 * 全てのリクエストで実行されるミドルウェアを登録する
 	 *
 	 * @var array
 	 */
@@ -20,14 +21,15 @@ class Kernel extends HttpKernel {
 
 	/**
 	 * The application's route middleware.
+	 * 特定のリクエストで実行するミドルウェアの短縮キーを登録する
 	 *
 	 * @var array
 	 */
 	protected $routeMiddleware = [
-		'auth' => 'App\Http\Middleware\Authenticate',
+		'auth'       => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
-		'old' => 'App\Http\Middleware\OldMiddleware',
+		'guest'      => 'App\Http\Middleware\RedirectIfAuthenticated',
+		'old'        => 'App\Http\Middleware\OldMiddleware',
 	];
 
 }
