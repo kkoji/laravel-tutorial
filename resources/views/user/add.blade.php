fbbde6dcca71836945f2ba07fb5e877ec95cc1d4
Kazuya Kojima <ypcjh662@yahoo.co.jp>
2015-06-09 18:56:05 +0900

    レイアウト微修正＆使っていないViewを削除

--- resources/views/user/add.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('common.layout')
-
-@section('content')
-	<h1>ユーザー新規追加</h1>
-	{!! Form::open() !!}
-	{!! Form::text('title', null, ['class' => 'form-control']) !!}
-	{!! Form::close() !!}
-@stop
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,8 +1,9 @@
 @extends('common.layout')
 
 @section('content')
-	<h1>ユーザー新規追加</h1>
-	{!! Form::model(new App\User, ['route' => 'user.store']) !!}
+	<div class="row">
+		<h1>ユーザー新規追加</h1>
+		{!! Form::model(new App\User, ['route' => 'user.store']) !!}
 		<div class="form-group">
 			{!! Form::label('name', 'Name:') !!}
 			{!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter name']) !!}
@@ -12,5 +13,6 @@
 			{!! Form::textarea('profile', null, ['class' => 'form-control', 'placeholder' => 'Enter profile']) !!}
 		</div>
 		{!! Form::submit('Add user', ['class' => 'btn btn-primary form-control']) !!}
-	{!! Form::close() !!}
+		{!! Form::close() !!}
+	</div>
 @stop
